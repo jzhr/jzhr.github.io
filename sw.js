c2b4512e9a863e4830db9aba5972bd66e0310b8b
Jason Zheng <one@fish.cat>
2020-11-04 21:02:26 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6b1672fc176ec4a26d6e.js"
+    "url": "webpack-runtime-8fb368b0e0e62202e68e.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cc480df1441d2150717e06d480b7e4dc"
+    "revision": "89b675f93b4371abd1566ed27edd7bc6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
