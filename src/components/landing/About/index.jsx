2a8fa322e9c78d13bf33425744c7f632058bf8bb
Jason Zheng <one@fish.cat>
2020-12-23 09:32:04 -0500

    small edits

--- src/components/landing/About/index.jsx
+++ src/components/landing/About/index.jsx
@@ -15,7 +15,7 @@ export const About = () => (
           I'm currently in my third year at Cornell University, pursuing a double major in Information Science and
           Statistical Science.
         </p>
-        <p>Originally born in Canada, I've lived briefly in California and now reside in Avon, Connecticut.</p>
+        <p>Originally from Canada, I've lived briefly in California and now reside in Avon, Connecticut.</p>
         <p>
           My interests include full-stack web development, UI/UX design, and data science. In my spare time, you'll find me reading good books,
           producing music, or hanging out with friends.
